67329397744eaa62937a6624f083e026342b06f2
Kate Young <89363807+katemyoung@users.noreply.github.com>
2021-10-21 21:25:54 +0100

    Update formatting exercise_1_spec.rb

--- spec/exercise_1_spec.rb
+++ spec/exercise_1_spec.rb
@@ -2,10 +2,8 @@ require 'exercise_1.rb' # this tells RSpec where to look for the code to test
 
 
 describe nothing do # this is a describe block 
-
-# this is a unit test, we call an it block here in RSpec
-it 'returns nil' do 
-expect(nothing).to eq nil 
-end
-
+  # this is a unit test, we call an it block here in RSpec
+  it 'returns nil' do 
+    expect(nothing).to eq nil 
+  end
 end
